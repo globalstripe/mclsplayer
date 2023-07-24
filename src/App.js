36f982327c014a3160505fd37a57c07db095b9e4
globalstripe <chris.clark@globalstripe.com>
2023-07-24 15:23:47 +0100

    added github pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {Player, PlayerStateContextProvider, AnnotationsContextProvider } from '@mycujoo/player-mls'
 
+
+// https://www.npmjs.com/package/@mycujoo/player-mls
+
+// Had to install
+// yarn add @mycujoo/player-mls
+// yarn install @emotion/react
+// And move Player {PlayerStateContextProvider} inside the {}  {Player, PlayerStateContextProvider, AnnotationsContextProvider }
+
+
+// Advanced https://devstreaming-cdn.apple.com/videos/streaming/examples/adv_dv_atmos/main.m3u8
+// Baisc https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <PlayerStateContextProvider>
+    <AnnotationsContextProvider>
+      <Player
+      autoplay={true}
+      qualitySelector={true}
+      
+      source="https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8" />
+    </AnnotationsContextProvider>
+  </PlayerStateContextProvider>
+      
     </div>
   );
 }
